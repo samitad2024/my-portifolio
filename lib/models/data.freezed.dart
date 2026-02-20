0c31429ce155f3842db9a16bb688d2b10fe1f721
samitad2024 <samigo2024@gmail.com>
2026-02-20 23:58:39 +0300

    chore: update lib/models/data.freezed.dart

--- lib/models/data.freezed.dart
+++ lib/models/data.freezed.dart
@@ -1,5 +1,5 @@
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -9,37 +9,62 @@ part of 'data.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
-
-Data _$DataFromJson(Map<String, dynamic> json) {
-  return _Data.fromJson(json);
-}
 
 /// @nodoc
 mixin _$Data {
-  Basic get basic => throw _privateConstructorUsedError;
-  List<Social> get socials => throw _privateConstructorUsedError;
-  About get about => throw _privateConstructorUsedError;
-  List<ServiceData> get services => throw _privateConstructorUsedError;
-  List<Project> get projects => throw _privateConstructorUsedError;
-  List<Contact> get contact => throw _privateConstructorUsedError;
-
-  /// Serializes this Data to a JSON map.
-  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  Basic get basic;
+  List<Social> get socials;
+  About get about;
+  List<ServiceData> get services;
+  List<Project> get projects;
+  List<Contact> get contact;
 
   /// Create a copy of Data
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $DataCopyWith<Data> get copyWith => throw _privateConstructorUsedError;
+  @pragma('vm:prefer-inline')
+  $DataCopyWith<Data> get copyWith =>
+      _$DataCopyWithImpl<Data>(this as Data, _$identity);
+
+  /// Serializes this Data to a JSON map.
+  Map<String, dynamic> toJson();
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is Data &&
+            (identical(other.basic, basic) || other.basic == basic) &&
+            const DeepCollectionEquality().equals(other.socials, socials) &&
+            (identical(other.about, about) || other.about == about) &&
+            const DeepCollectionEquality().equals(other.services, services) &&
+            const DeepCollectionEquality().equals(other.projects, projects) &&
+            const DeepCollectionEquality().equals(other.contact, contact));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      basic,
+      const DeepCollectionEquality().hash(socials),
+      about,
+      const DeepCollectionEquality().hash(services),
+      const DeepCollectionEquality().hash(projects),
+      const DeepCollectionEquality().hash(contact));
+
+  @override
+  String toString() {
+    return 'Data(basic: $basic, socials: $socials, about: $about, services: $services, projects: $projects, contact: $contact)';
+  }
 }
 
 /// @nodoc
-abstract class $DataCopyWith<$Res> {
-  factory $DataCopyWith(Data value, $Res Function(Data) then) =
-      _$DataCopyWithImpl<$Res, Data>;
+abstract mixin class $DataCopyWith<$Res> {
+  factory $DataCopyWith(Data value, $Res Function(Data) _then) =
+      _$DataCopyWithImpl;
   @useResult
   $Res call(
       {Basic basic,
@@ -54,14 +79,11 @@ abstract class $DataCopyWith<$Res> {
 }
 
 /// @nodoc
-class _$DataCopyWithImpl<$Res, $Val extends Data>
-    implements $DataCopyWith<$Res> {
-  _$DataCopyWithImpl(this._value, this._then);
+class _$DataCopyWithImpl<$Res> implements $DataCopyWith<$Res> {
+  _$DataCopyWithImpl(this._self, this._then);
 
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
+  final Data _self;
+  final $Res Function(Data) _then;
 
   /// Create a copy of Data
   /// with the given fields replaced by the non-null parameter values.
@@ -75,32 +97,32 @@ class _$DataCopyWithImpl<$Res, $Val extends Data>
     Object? projects = null,
     Object? contact = null,
   }) {
-    return _then(_value.copyWith(
+    return _then(_self.copyWith(
       basic: null == basic
-          ? _value.basic
+          ? _self.basic
           : basic // ignore: cast_nullable_to_non_nullable
               as Basic,
       socials: null == socials
-          ? _value.socials
+          ? _self.socials
           : socials // ignore: cast_nullable_to_non_nullable
               as List<Social>,
       about: null == about
-          ? _value.about
+          ? _self.about
           : about // ignore: cast_nullable_to_non_nullable
               as About,
       services: null == services
-          ? _value.services
+          ? _self.services
           : services // ignore: cast_nullable_to_non_nullable
               as List<ServiceData>,
       projects: null == projects
-          ? _value.projects
+          ? _self.projects
           : projects // ignore: cast_nullable_to_non_nullable
               as List<Project>,
       contact: null == contact
-          ? _value.contact
+          ? _self.contact
           : contact // ignore: cast_nullable_to_non_nullable
               as List<Contact>,
-    ) as $Val);
+    ));
   }
 
   /// Create a copy of Data
@@ -108,8 +130,8 @@ class _$DataCopyWithImpl<$Res, $Val extends Data>
   @override
   @pragma('vm:prefer-inline')
   $BasicCopyWith<$Res> get basic {
-    return $BasicCopyWith<$Res>(_value.basic, (value) {
-      return _then(_value.copyWith(basic: value) as $Val);
+    return $BasicCopyWith<$Res>(_self.basic, (value) {
+      return _then(_self.copyWith(basic: value));
     });
   }
 
@@ -118,85 +140,197 @@ class _$DataCopyWithImpl<$Res, $Val extends Data>
   @override
   @pragma('vm:prefer-inline')
   $AboutCopyWith<$Res> get about {
-    return $AboutCopyWith<$Res>(_value.about, (value) {
-      return _then(_value.copyWith(about: value) as $Val);
+    return $AboutCopyWith<$Res>(_self.about, (value) {
+      return _then(_self.copyWith(about: value));
     });
   }
 }
 
-/// @nodoc
-abstract class _$$DataImplCopyWith<$Res> implements $DataCopyWith<$Res> {
-  factory _$$DataImplCopyWith(
-          _$DataImpl value, $Res Function(_$DataImpl) then) =
-      __$$DataImplCopyWithImpl<$Res>;
-  @override
-  @useResult
-  $Res call(
-      {Basic basic,
-      List<Social> socials,
-      About about,
-      List<ServiceData> services,
-      List<Project> projects,
-      List<Contact> contact});
+/// Adds pattern-matching-related methods to [Data].
+extension DataPatterns on Data {
+  /// A variant of `map` that fallback to returning `orElse`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
 
-  @override
-  $BasicCopyWith<$Res> get basic;
-  @override
-  $AboutCopyWith<$Res> get about;
-}
-
-/// @nodoc
-class __$$DataImplCopyWithImpl<$Res>
-    extends _$DataCopyWithImpl<$Res, _$DataImpl>
-    implements _$$DataImplCopyWith<$Res> {
-  __$$DataImplCopyWithImpl(_$DataImpl _value, $Res Function(_$DataImpl) _then)
-      : super(_value, _then);
-
-  /// Create a copy of Data
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? basic = null,
-    Object? socials = null,
-    Object? about = null,
-    Object? services = null,
-    Object? projects = null,
-    Object? contact = null,
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_Data value)? $default, {
+    required TResult orElse(),
   }) {
-    return _then(_$DataImpl(
-      basic: null == basic
-          ? _value.basic
-          : basic // ignore: cast_nullable_to_non_nullable
-              as Basic,
-      socials: null == socials
-          ? _value._socials
-          : socials // ignore: cast_nullable_to_non_nullable
-              as List<Social>,
-      about: null == about
-          ? _value.about
-          : about // ignore: cast_nullable_to_non_nullable
-              as About,
-      services: null == services
-          ? _value._services
-          : services // ignore: cast_nullable_to_non_nullable
-              as List<ServiceData>,
-      projects: null == projects
-          ? _value._projects
-          : projects // ignore: cast_nullable_to_non_nullable
-              as List<Project>,
-      contact: null == contact
-          ? _value._contact
-          : contact // ignore: cast_nullable_to_non_nullable
-              as List<Contact>,
-    ));
+    final _that = this;
+    switch (_that) {
+      case _Data() when $default != null:
+        return $default(_that);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// Callbacks receives the raw object, upcasted.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case final Subclass2 value:
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_Data value) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Data():
+        return $default(_that);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `map` that fallback to returning `null`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_Data value)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Data() when $default != null:
+        return $default(_that);
+      case _:
+        return null;
+    }
+  }
+
+  /// A variant of `when` that fallback to an `orElse` callback.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function(
+            Basic basic,
+            List<Social> socials,
+            About about,
+            List<ServiceData> services,
+            List<Project> projects,
+            List<Contact> contact)?
+        $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _Data() when $default != null:
+        return $default(_that.basic, _that.socials, _that.about, _that.services,
+            _that.projects, _that.contact);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// As opposed to `map`, this offers destructuring.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case Subclass2(:final field2):
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function(
+            Basic basic,
+            List<Social> socials,
+            About about,
+            List<ServiceData> services,
+            List<Project> projects,
+            List<Contact> contact)
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Data():
+        return $default(_that.basic, _that.socials, _that.about, _that.services,
+            _that.projects, _that.contact);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `when` that fallback to returning `null`
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function(
+            Basic basic,
+            List<Social> socials,
+            About about,
+            List<ServiceData> services,
+            List<Project> projects,
+            List<Contact> contact)?
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Data() when $default != null:
+        return $default(_that.basic, _that.socials, _that.about, _that.services,
+            _that.projects, _that.contact);
+      case _:
+        return null;
+    }
   }
 }
 
 /// @nodoc
 @JsonSerializable()
-class _$DataImpl implements _Data {
-  const _$DataImpl(
+class _Data implements Data {
+  const _Data(
       {required this.basic,
       required final List<Social> socials,
       required this.about,
@@ -207,9 +341,7 @@ class _$DataImpl implements _Data {
         _services = services,
         _projects = projects,
         _contact = contact;
-
-  factory _$DataImpl.fromJson(Map<String, dynamic> json) =>
-      _$$DataImplFromJson(json);
+  factory _Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
 
   @override
   final Basic basic;
@@ -247,16 +379,26 @@ class _$DataImpl implements _Data {
     return EqualUnmodifiableListView(_contact);
   }
 
+  /// Create a copy of Data
+  /// with the given fields replaced by the non-null parameter values.
   @override
-  String toString() {
-    return 'Data(basic: $basic, socials: $socials, about: $about, services: $services, projects: $projects, contact: $contact)';
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @pragma('vm:prefer-inline')
+  _$DataCopyWith<_Data> get copyWith =>
+      __$DataCopyWithImpl<_Data>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$DataToJson(
+      this,
+    );
   }
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$DataImpl &&
+            other is _Data &&
             (identical(other.basic, basic) || other.basic == basic) &&
             const DeepCollectionEquality().equals(other._socials, _socials) &&
             (identical(other.about, about) || other.about == about) &&
@@ -276,50 +418,98 @@ class _$DataImpl implements _Data {
       const DeepCollectionEquality().hash(_projects),
       const DeepCollectionEquality().hash(_contact));
 
-  /// Create a copy of Data
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
   @override
-  @pragma('vm:prefer-inline')
-  _$$DataImplCopyWith<_$DataImpl> get copyWith =>
-      __$$DataImplCopyWithImpl<_$DataImpl>(this, _$identity);
-
-  @override
-  Map<String, dynamic> toJson() {
-    return _$$DataImplToJson(
-      this,
-    );
+  String toString() {
+    return 'Data(basic: $basic, socials: $socials, about: $about, services: $services, projects: $projects, contact: $contact)';
   }
 }
 
-abstract class _Data implements Data {
-  const factory _Data(
-      {required final Basic basic,
-      required final List<Social> socials,
-      required final About about,
-      required final List<ServiceData> services,
-      required final List<Project> projects,
-      required final List<Contact> contact}) = _$DataImpl;
+/// @nodoc
+abstract mixin class _$DataCopyWith<$Res> implements $DataCopyWith<$Res> {
+  factory _$DataCopyWith(_Data value, $Res Function(_Data) _then) =
+      __$DataCopyWithImpl;
+  @override
+  @useResult
+  $Res call(
+      {Basic basic,
+      List<Social> socials,
+      About about,
+      List<ServiceData> services,
+      List<Project> projects,
+      List<Contact> contact});
 
-  factory _Data.fromJson(Map<String, dynamic> json) = _$DataImpl.fromJson;
+  @override
+  $BasicCopyWith<$Res> get basic;
+  @override
+  $AboutCopyWith<$Res> get about;
+}
 
-  @override
-  Basic get basic;
-  @override
-  List<Social> get socials;
-  @override
-  About get about;
-  @override
-  List<ServiceData> get services;
-  @override
-  List<Project> get projects;
-  @override
-  List<Contact> get contact;
+/// @nodoc
+class __$DataCopyWithImpl<$Res> implements _$DataCopyWith<$Res> {
+  __$DataCopyWithImpl(this._self, this._then);
+
+  final _Data _self;
+  final $Res Function(_Data) _then;
 
   /// Create a copy of Data
   /// with the given fields replaced by the non-null parameter values.
   @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$DataImplCopyWith<_$DataImpl> get copyWith =>
-      throw _privateConstructorUsedError;
+  @pragma('vm:prefer-inline')
+  $Res call({
+    Object? basic = null,
+    Object? socials = null,
+    Object? about = null,
+    Object? services = null,
+    Object? projects = null,
+    Object? contact = null,
+  }) {
+    return _then(_Data(
+      basic: null == basic
+          ? _self.basic
+          : basic // ignore: cast_nullable_to_non_nullable
+              as Basic,
+      socials: null == socials
+          ? _self._socials
+          : socials // ignore: cast_nullable_to_non_nullable
+              as List<Social>,
+      about: null == about
+          ? _self.about
+          : about // ignore: cast_nullable_to_non_nullable
+              as About,
+      services: null == services
+          ? _self._services
+          : services // ignore: cast_nullable_to_non_nullable
+              as List<ServiceData>,
+      projects: null == projects
+          ? _self._projects
+          : projects // ignore: cast_nullable_to_non_nullable
+              as List<Project>,
+      contact: null == contact
+          ? _self._contact
+          : contact // ignore: cast_nullable_to_non_nullable
+              as List<Contact>,
+    ));
+  }
+
+  /// Create a copy of Data
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @pragma('vm:prefer-inline')
+  $BasicCopyWith<$Res> get basic {
+    return $BasicCopyWith<$Res>(_self.basic, (value) {
+      return _then(_self.copyWith(basic: value));
+    });
+  }
+
+  /// Create a copy of Data
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @pragma('vm:prefer-inline')
+  $AboutCopyWith<$Res> get about {
+    return $AboutCopyWith<$Res>(_self.about, (value) {
+      return _then(_self.copyWith(about: value));
+    });
+  }
 }
+
+// dart format on
