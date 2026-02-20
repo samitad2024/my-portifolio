2ddaa6f8162c6d4f081f1733788d22142d5c5166
samitad2024 <samigo2024@gmail.com>
2026-02-20 23:58:09 +0300

    chore: update lib/components/nav_bar.dart

--- lib/components/nav_bar.dart
+++ lib/components/nav_bar.dart
@@ -1,6 +1,7 @@
 import 'package:devfolio/components/app_button.dart';
 import 'package:devfolio/constants/theme.dart';
 import 'package:jaspr/jaspr.dart';
+import 'package:jaspr/dom.dart';
 
 @client
 class NavBar extends StatelessComponent {
@@ -15,41 +16,11 @@ class NavBar extends StatelessComponent {
         span([text(' />')]),
       ]),
       div(classes: 'labels', [
-        a(
-          classes: 'navbar-label',
-          href: '#',
-          [
-            text('HOME'),
-          ],
-        ),
-        a(
-          href: '#about',
-          classes: 'navbar-label',
-          [
-            text('ABOUT'),
-          ],
-        ),
-        a(
-          href: '#services',
-          classes: 'navbar-label',
-          [
-            text('SERVICES'),
-          ],
-        ),
-        a(
-          href: '#projects',
-          classes: 'navbar-label',
-          [
-            text('PROJECTS'),
-          ],
-        ),
-        a(
-          href: '#contact',
-          classes: 'navbar-label',
-          [
-            text('CONTACT'),
-          ],
-        ),
+        a(classes: 'navbar-label', href: '#', [text('HOME')]),
+        a(href: '#about', classes: 'navbar-label', [text('ABOUT')]),
+        a(href: '#services', classes: 'navbar-label', [text('SERVICES')]),
+        a(href: '#projects', classes: 'navbar-label', [text('PROJECTS')]),
+        a(href: '#contact', classes: 'navbar-label', [text('CONTACT')]),
         AppButton(
           label: 'RESUME',
           href:
@@ -59,7 +30,6 @@ class NavBar extends StatelessComponent {
     ]);
   }
 
-  @css
   static final List<StyleRule> styles = [
     css('.navbar')
         .box(padding: EdgeInsets.all(25.px))
@@ -68,36 +38,21 @@ class NavBar extends StatelessComponent {
           justifyContent: JustifyContent.spaceBetween,
           alignItems: AlignItems.center,
         )
-        .background(
-          color: Colors.black,
-        ),
+        .background(color: Colors.black),
     css('.navbar-label')
-        .box(
-          padding: EdgeInsets.only(right: 35.px),
-        )
-        .text(
-          fontSize: 12.px,
-          decoration: TextDecoration.none,
-        )
+        .box(padding: EdgeInsets.only(right: 35.px))
+        .text(fontSize: 12.px, decoration: TextDecoration.none)
         .flexbox(
           direction: FlexDirection.row,
           justifyContent: JustifyContent.spaceBetween,
           alignItems: AlignItems.center,
         ),
     css('.navbar-label:hover')
-        .text(
-          color: themePrimaryColor,
-        )
-        .box(
-          transition: Transition('color', duration: 500),
-        ),
-    css('.labels').flexbox(
-      direction: FlexDirection.row,
-      justifyContent: JustifyContent.end,
-    ),
-    css('.brand').text(
-      fontFamily: FontFamily('Agustina'),
-      fontSize: 24.px,
-    ),
+        .text(color: themePrimaryColor)
+        .box(transition: Transition('color', duration: 500)),
+    css(
+      '.labels',
+    ).flexbox(direction: FlexDirection.row, justifyContent: JustifyContent.end),
+    css('.brand').text(fontFamily: FontFamily('Agustina'), fontSize: 24.px),
   ];
 }
