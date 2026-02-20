c7139c4d001fa668f2f5454a374928061983080c
samitad2024 <samigo2024@gmail.com>
2026-02-20 23:58:08 +0300

    chore: update lib/components/footer.dart

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -1,4 +1,5 @@
 import 'package:jaspr/jaspr.dart';
+import 'package:jaspr/dom.dart';
 
 @client
 class Footer extends StatelessComponent {
@@ -9,15 +10,15 @@ class Footer extends StatelessComponent {
     yield footer(classes: 'foot', [
       span([text('Developed in 💙 with')]),
       a(
-          href: 'https://docs.page/schultek/jaspr',
-          classes: 'tech-name',
-          target: Target.blank,
-          styles: Styles.text(color: Color.hex('#1977d1')),
-          [text('Jaspr')]),
+        href: 'https://docs.page/schultek/jaspr',
+        classes: 'tech-name',
+        target: Target.blank,
+        styles: Styles.text(color: Color.hex('#1977d1')),
+        [text('Jaspr')],
+      ),
     ]);
   }
 
-  @css
   static final List<StyleRule> styles = [
     css('.foot')
         .flexbox(
@@ -25,15 +26,10 @@ class Footer extends StatelessComponent {
           alignItems: AlignItems.center,
           justifyContent: JustifyContent.center,
         )
-        .box(
-          padding: EdgeInsets.symmetric(vertical: 2.vh),
-        )
+        .box(padding: EdgeInsets.symmetric(vertical: 2.vh))
         .text(fontSize: 12.px),
     css('.tech-name')
-        .text(
-          fontSize: 12.px,
-          decoration: TextDecoration.none,
-        )
+        .text(fontSize: 12.px, decoration: TextDecoration.none)
         .box(margin: EdgeInsets.only(left: 5.px)),
   ];
 }
