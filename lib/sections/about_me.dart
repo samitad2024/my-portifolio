772de8a5dc4195acd2374be9795868ddca4891a4
samitad2024 <samigo2024@gmail.com>
2026-02-21 00:56:32 +0300

    chore: update lib/sections/about_me.dart

--- lib/sections/about_me.dart
+++ lib/sections/about_me.dart
@@ -4,121 +4,87 @@ import 'package:devfolio/models/about.dart';
 import 'package:devfolio/models/basic.dart';
 import 'package:devfolio/utils/assets.dart';
 import 'package:jaspr/jaspr.dart';
+import 'package:jaspr/dom.dart';
 
-@client
 class AboutMeSection extends StatelessComponent {
   final About about;
   final Basic basic;
-  const AboutMeSection({
-    super.key,
-    required this.about,
-    required this.basic,
-  });
+  const AboutMeSection({super.key, required this.about, required this.basic});
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
     yield section(classes: 'about-me-section', [
-      span(classes: 'title', id: 'about', [
-        text('About Me'),
-      ]),
-      span(classes: 'subtitle', [
-        text('Get to know me :)'),
-      ]),
+      span(classes: 'title', id: 'about', [text('About Me')]),
+      span(classes: 'subtitle', [text('Get to know me :)')]),
       div(classes: 'about-section-body', [
         div(classes: 'profile-picture', []),
         div(classes: 'about-details', [
           span(
-              classes: 'intro-label',
-              styles: Styles.text(color: themePrimaryColor),
-              [
-                text('Who am I?'),
-              ]),
-          span(classes: 'intro-heading', [
-            text(about.heading),
-          ]),
+            classes: 'intro-label',
+            styles: Styles.text(color: themePrimaryColor),
+            [text('Who am I?')],
+          ),
+          span(classes: 'intro-heading', [text(about.heading)]),
           span(
-              classes: 'intro-details',
-              styles: Styles.text(
-                color: Colors.lightGrey,
-              ),
-              [
-                text(about.description),
-              ]),
+            classes: 'intro-details',
+            styles: Styles.text(color: Colors.lightGrey),
+            [text(about.description)],
+          ),
           div(classes: 'divider', []),
           span(
-              classes: 'tech-label',
-              styles: Styles.text(color: themePrimaryColor),
-              [
-                text('Technologies I have worked with:'),
-              ]),
+            classes: 'tech-label',
+            styles: Styles.text(color: themePrimaryColor),
+            [text('Technologies I have worked with:')],
+          ),
           div(classes: 'tech-stack', [
             for (final t in about.tech)
               span([
                 i(classes: 'fa-solid fa-play play-icon', []),
-                span(classes: 'tech-item', [
-                  text(t),
-                ]),
+                span(classes: 'tech-item', [text(t)]),
               ]),
           ]),
           div(classes: 'divider', []),
           div(classes: 'personal-row', [
             div([
-              span(classes: 'personal-label', [
-                text('Name: '),
-              ]),
+              span(classes: 'personal-label', [text('Name: ')]),
               span(classes: 'personal-value', [
                 text('${basic.firstName} ${basic.lastName}'),
               ]),
             ]),
             div([
-              span(classes: 'personal-label', [
-                text('Email: '),
-              ]),
-              span(classes: 'personal-value', [
-                text(basic.email),
-              ]),
-            ])
+              span(classes: 'personal-label', [text('Email: ')]),
+              span(classes: 'personal-value', [text(basic.email)]),
+            ]),
           ]),
           div(styles: Styles.box(height: 15.px), []),
           div(classes: 'personal-row', [
             div([
-              span(classes: 'personal-label', [
-                text('Age: '),
-              ]),
-              span(classes: 'personal-value', [
-                text(basic.age.toString()),
-              ]),
+              span(classes: 'personal-label', [text('Age: ')]),
+              span(classes: 'personal-value', [text(basic.age.toString())]),
             ]),
             div([
-              span(classes: 'personal-label', [
-                text('From: '),
-              ]),
-              span(classes: 'personal-value', [
-                text(basic.address),
-              ]),
-            ])
+              span(classes: 'personal-label', [text('From: ')]),
+              span(classes: 'personal-value', [text(basic.address)]),
+            ]),
           ]),
           div(styles: Styles.box(height: 25.px), []),
           div(classes: 'work-row', [
-            AppButton(
-              label: 'RESUME',
-              href: basic.resume,
+            AppButton(label: 'RESUME', href: basic.resume),
+            div(
+              classes: 'divider',
+              styles: Styles.box(width: 80.px),
+              [],
             ),
-            div(classes: 'divider', styles: Styles.box(width: 80.px), []),
             for (final work in about.works)
               a(classes: 'work-item', href: work.url, target: Target.blank, [
-                img(
-                  src: work.image,
-                  height: work.height,
-                ),
+                img(src: work.image, height: work.height),
               ]),
-          ])
+          ]),
         ]),
-      ])
+      ]),
     ]);
   }
 
-  @css
   static final List<StyleRule> styles = [
     css('.about-me-section')
         .flexbox(
@@ -130,12 +96,14 @@ class AboutMeSection extends StatelessComponent {
           // padding: EdgeInsets.only(top: 5.vh),
           width: 100.percent,
         ),
-    css('.play-icon').text(color: themePrimaryColor),
-    css('.title').text(
-      fontFamily: FontFamily('Montserrat'),
-      fontWeight: FontWeight.w100,
-      fontSize: 40.px,
-    ),
+    css('.play-icon', [css.text(color: themePrimaryColor)]),
+    css('.title', [
+      css.text(
+        fontFamily: FontFamily('Montserrat'),
+        fontWeight: FontWeight.w100,
+        fontSize: 40.px,
+      ),
+    ]),
     css('.about-section-body')
         .flexbox(
           direction: FlexDirection.row,
@@ -148,9 +116,7 @@ class AboutMeSection extends StatelessComponent {
         ),
     css('.profile-picture')
         .background(
-          image: ImageStyle.url(
-            StaticAssets.coloredImage,
-          ),
+          image: ImageStyle.url(StaticAssets.coloredImage),
           size: BackgroundSize.cover,
         )
         .box(
@@ -164,20 +130,15 @@ class AboutMeSection extends StatelessComponent {
           alignItems: AlignItems.start,
           justifyContent: JustifyContent.center,
         )
-        .box(
-          width: 750.px,
-        ),
-    css('.intro-label').text(fontSize: 18.px),
-    css('.intro-heading')
-        .box(
-          margin: EdgeInsets.symmetric(vertical: 15.px),
-        )
-        .text(fontSize: 20.px),
-    css('.intro-details').text(
-      lineHeight: 3.vh,
-      wordSpacing: 1.5.px,
-      color: Colors.lightGrey,
-    ),
+        .box(width: 750.px),
+    css('.intro-label', [css.text(fontSize: 18.px)]),
+    css('.intro-heading', [
+      css.box(margin: EdgeInsets.symmetric(vertical: 15.px)),
+      css.text(fontSize: 20.px),
+    ]),
+    css('.intro-details', [
+      css.text(lineHeight: 3.vh, wordSpacing: 1.5.px, color: Colors.lightGrey),
+    ]),
     css('.divider')
         .box(
           height: 1.px,
@@ -185,7 +146,7 @@ class AboutMeSection extends StatelessComponent {
           margin: EdgeInsets.symmetric(vertical: 2.vh),
         )
         .background(color: Colors.lightGrey),
-    css('.tech-label').text(fontSize: 12.px),
+    css('.tech-label', [css.text(fontSize: 12.px)]),
     css('.tech-stack')
         .flexbox(
           direction: FlexDirection.row,
@@ -193,24 +154,26 @@ class AboutMeSection extends StatelessComponent {
           justifyContent: JustifyContent.start,
           wrap: FlexWrap.wrap,
         )
-        .box(
-          margin: EdgeInsets.only(top: 15.px),
-        ),
-    css('.tech-item')
-        .box(margin: EdgeInsets.only(right: 12.px, left: 5.px))
-        .text(fontSize: 14.px),
-    css('.personal-label').text(
-      fontWeight: FontWeight.bold,
-      fontSize: 12.px,
-    ),
-    css('.personal-value').text(
-      fontSize: 12.px,
-    ),
-    css('.personal-row').box(width: 100.percent).flexbox(
+        .box(margin: EdgeInsets.only(top: 15.px)),
+    css('.tech-item', [
+      css.box(
+        margin: EdgeInsets.only(right: 12.px, left: 5.px),
+      ),
+      css.text(fontSize: 14.px),
+    ]),
+    css('.personal-label', [
+      css.text(fontWeight: FontWeight.bold, fontSize: 12.px),
+    ]),
+    css('.personal-value', [css.text(fontSize: 12.px)]),
+    css('.personal-row')
+        .box(width: 100.percent)
+        .flexbox(
           direction: FlexDirection.row,
           justifyContent: JustifyContent.spaceBetween,
         ),
-    css('.work-row').box(width: 100.percent).flexbox(
+    css('.work-row')
+        .box(width: 100.percent)
+        .flexbox(
           direction: FlexDirection.row,
           alignItems: AlignItems.center,
           justifyContent: JustifyContent.spaceBetween,
@@ -246,17 +209,14 @@ class AboutMeSection extends StatelessComponent {
       ),
       css('.profile-picture')
           .background(
-            image: ImageStyle.url(
-              StaticAssets.mobileImage,
-            ),
+            image: ImageStyle.url(StaticAssets.mobileImage),
             size: BackgroundSize.cover,
           )
-          .box(
-            width: 250.px,
-            height: 250.px,
-          ),
-      css('.about-details')
-          .box(width: 80.percent, margin: EdgeInsets.only(top: 40.px)),
+          .box(width: 250.px, height: 250.px),
+      css('.about-details').box(
+        width: 80.percent,
+        margin: EdgeInsets.only(top: 40.px),
+      ),
     ]),
   ];
 }
