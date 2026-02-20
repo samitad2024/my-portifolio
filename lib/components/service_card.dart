5fc8d242ce982d7fd0e4994cc40af1dda3f9f664
samitad2024 <samigo2024@gmail.com>
2026-02-20 23:58:13 +0300

    chore: update lib/components/service_card.dart

--- lib/components/service_card.dart
+++ lib/components/service_card.dart
@@ -1,27 +1,21 @@
 import 'package:devfolio/constants/theme.dart';
 import 'package:jaspr/jaspr.dart';
+import 'package:jaspr/dom.dart';
 
 @client
 class ServiceCard extends StatelessComponent {
   final String icon;
   final String label;
-  const ServiceCard({
-    super.key,
-    required this.icon,
-    required this.label,
-  });
+  const ServiceCard({super.key, required this.icon, required this.label});
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
     yield div(classes: 'service-card', [
       img(src: icon, height: 80),
-      span(classes: 'service-title', [
-        text(label),
-      ]),
+      span(classes: 'service-title', [text(label)]),
     ]);
   }
 
-  @css
   static final List<StyleRule> styles = [
     css('.service-card')
         .flexbox(
@@ -36,16 +30,10 @@ class ServiceCard extends StatelessComponent {
           radius: BorderRadius.circular(12.px),
           margin: EdgeInsets.only(top: 25.px, left: 15.px, right: 15.px),
         )
-        .background(
-          color: themeDarkGreyColor,
-        ),
-    css('.service-title')
-        .text(
-          fontSize: 14.px,
-        )
-        .box(
-          margin: EdgeInsets.only(top: 10.px),
-        ),
+        .background(color: themeDarkGreyColor),
+    css(
+      '.service-title',
+    ).text(fontSize: 14.px).box(margin: EdgeInsets.only(top: 10.px)),
     css('.service-card:hover').box(
       shadow: BoxShadow(
         color: themePrimaryColor,
