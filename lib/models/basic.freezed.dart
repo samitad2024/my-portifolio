ff68b96d620538a1668f05f52b17f756d1352b8c
samitad2024 <samigo2024@gmail.com>
2026-02-20 23:58:26 +0300

    chore: update lib/models/basic.freezed.dart

--- lib/models/basic.freezed.dart
+++ lib/models/basic.freezed.dart
@@ -1,5 +1,5 @@
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -9,118 +9,62 @@ part of 'basic.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
-
-Basic _$BasicFromJson(Map<String, dynamic> json) {
-  return _Basic.fromJson(json);
-}
 
 /// @nodoc
 mixin _$Basic {
-  String get firstName => throw _privateConstructorUsedError;
-  String get lastName => throw _privateConstructorUsedError;
-  int get age => throw _privateConstructorUsedError;
-  String get email => throw _privateConstructorUsedError;
-  String get phone => throw _privateConstructorUsedError;
-  String get address => throw _privateConstructorUsedError;
-  List<String> get photos => throw _privateConstructorUsedError;
-  String get resume => throw _privateConstructorUsedError;
-
-  /// Serializes this Basic to a JSON map.
-  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  String get firstName;
+  String get lastName;
+  int get age;
+  String get email;
+  String get phone;
+  String get address;
+  List<String> get photos;
+  String get resume;
 
   /// Create a copy of Basic
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $BasicCopyWith<Basic> get copyWith => throw _privateConstructorUsedError;
-}
-
-/// @nodoc
-abstract class $BasicCopyWith<$Res> {
-  factory $BasicCopyWith(Basic value, $Res Function(Basic) then) =
-      _$BasicCopyWithImpl<$Res, Basic>;
-  @useResult
-  $Res call(
-      {String firstName,
-      String lastName,
-      int age,
-      String email,
-      String phone,
-      String address,
-      List<String> photos,
-      String resume});
-}
-
-/// @nodoc
-class _$BasicCopyWithImpl<$Res, $Val extends Basic>
-    implements $BasicCopyWith<$Res> {
-  _$BasicCopyWithImpl(this._value, this._then);
-
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
-
-  /// Create a copy of Basic
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
+  $BasicCopyWith<Basic> get copyWith =>
+      _$BasicCopyWithImpl<Basic>(this as Basic, _$identity);
+
+  /// Serializes this Basic to a JSON map.
+  Map<String, dynamic> toJson();
+
   @override
-  $Res call({
-    Object? firstName = null,
-    Object? lastName = null,
-    Object? age = null,
-    Object? email = null,
-    Object? phone = null,
-    Object? address = null,
-    Object? photos = null,
-    Object? resume = null,
-  }) {
-    return _then(_value.copyWith(
-      firstName: null == firstName
-          ? _value.firstName
-          : firstName // ignore: cast_nullable_to_non_nullable
-              as String,
-      lastName: null == lastName
-          ? _value.lastName
-          : lastName // ignore: cast_nullable_to_non_nullable
-              as String,
-      age: null == age
-          ? _value.age
-          : age // ignore: cast_nullable_to_non_nullable
-              as int,
-      email: null == email
-          ? _value.email
-          : email // ignore: cast_nullable_to_non_nullable
-              as String,
-      phone: null == phone
-          ? _value.phone
-          : phone // ignore: cast_nullable_to_non_nullable
-              as String,
-      address: null == address
-          ? _value.address
-          : address // ignore: cast_nullable_to_non_nullable
-              as String,
-      photos: null == photos
-          ? _value.photos
-          : photos // ignore: cast_nullable_to_non_nullable
-              as List<String>,
-      resume: null == resume
-          ? _value.resume
-          : resume // ignore: cast_nullable_to_non_nullable
-              as String,
-    ) as $Val);
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is Basic &&
+            (identical(other.firstName, firstName) ||
+                other.firstName == firstName) &&
+            (identical(other.lastName, lastName) ||
+                other.lastName == lastName) &&
+            (identical(other.age, age) || other.age == age) &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.phone, phone) || other.phone == phone) &&
+            (identical(other.address, address) || other.address == address) &&
+            const DeepCollectionEquality().equals(other.photos, photos) &&
+            (identical(other.resume, resume) || other.resume == resume));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(runtimeType, firstName, lastName, age, email,
+      phone, address, const DeepCollectionEquality().hash(photos), resume);
+
+  @override
+  String toString() {
+    return 'Basic(firstName: $firstName, lastName: $lastName, age: $age, email: $email, phone: $phone, address: $address, photos: $photos, resume: $resume)';
   }
 }
 
 /// @nodoc
-abstract class _$$BasicImplCopyWith<$Res> implements $BasicCopyWith<$Res> {
-  factory _$$BasicImplCopyWith(
-          _$BasicImpl value, $Res Function(_$BasicImpl) then) =
-      __$$BasicImplCopyWithImpl<$Res>;
-  @override
+abstract mixin class $BasicCopyWith<$Res> {
+  factory $BasicCopyWith(Basic value, $Res Function(Basic) _then) =
+      _$BasicCopyWithImpl;
   @useResult
   $Res call(
       {String firstName,
@@ -134,12 +78,11 @@ abstract class _$$BasicImplCopyWith<$Res> implements $BasicCopyWith<$Res> {
 }
 
 /// @nodoc
-class __$$BasicImplCopyWithImpl<$Res>
-    extends _$BasicCopyWithImpl<$Res, _$BasicImpl>
-    implements _$$BasicImplCopyWith<$Res> {
-  __$$BasicImplCopyWithImpl(
-      _$BasicImpl _value, $Res Function(_$BasicImpl) _then)
-      : super(_value, _then);
+class _$BasicCopyWithImpl<$Res> implements $BasicCopyWith<$Res> {
+  _$BasicCopyWithImpl(this._self, this._then);
+
+  final Basic _self;
+  final $Res Function(Basic) _then;
 
   /// Create a copy of Basic
   /// with the given fields replaced by the non-null parameter values.
@@ -155,47 +98,213 @@ class __$$BasicImplCopyWithImpl<$Res>
     Object? photos = null,
     Object? resume = null,
   }) {
-    return _then(_$BasicImpl(
+    return _then(_self.copyWith(
       firstName: null == firstName
-          ? _value.firstName
+          ? _self.firstName
           : firstName // ignore: cast_nullable_to_non_nullable
               as String,
       lastName: null == lastName
-          ? _value.lastName
+          ? _self.lastName
           : lastName // ignore: cast_nullable_to_non_nullable
               as String,
       age: null == age
-          ? _value.age
+          ? _self.age
           : age // ignore: cast_nullable_to_non_nullable
               as int,
       email: null == email
-          ? _value.email
+          ? _self.email
           : email // ignore: cast_nullable_to_non_nullable
               as String,
       phone: null == phone
-          ? _value.phone
+          ? _self.phone
           : phone // ignore: cast_nullable_to_non_nullable
               as String,
       address: null == address
-          ? _value.address
+          ? _self.address
           : address // ignore: cast_nullable_to_non_nullable
               as String,
       photos: null == photos
-          ? _value._photos
+          ? _self.photos
           : photos // ignore: cast_nullable_to_non_nullable
               as List<String>,
       resume: null == resume
-          ? _value.resume
+          ? _self.resume
           : resume // ignore: cast_nullable_to_non_nullable
               as String,
     ));
   }
 }
 
+/// Adds pattern-matching-related methods to [Basic].
+extension BasicPatterns on Basic {
+  /// A variant of `map` that fallback to returning `orElse`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_Basic value)? $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _Basic() when $default != null:
+        return $default(_that);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// Callbacks receives the raw object, upcasted.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case final Subclass2 value:
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_Basic value) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Basic():
+        return $default(_that);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `map` that fallback to returning `null`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_Basic value)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Basic() when $default != null:
+        return $default(_that);
+      case _:
+        return null;
+    }
+  }
+
+  /// A variant of `when` that fallback to an `orElse` callback.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function(String firstName, String lastName, int age, String email,
+            String phone, String address, List<String> photos, String resume)?
+        $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _Basic() when $default != null:
+        return $default(_that.firstName, _that.lastName, _that.age, _that.email,
+            _that.phone, _that.address, _that.photos, _that.resume);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// As opposed to `map`, this offers destructuring.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case Subclass2(:final field2):
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function(String firstName, String lastName, int age, String email,
+            String phone, String address, List<String> photos, String resume)
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Basic():
+        return $default(_that.firstName, _that.lastName, _that.age, _that.email,
+            _that.phone, _that.address, _that.photos, _that.resume);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `when` that fallback to returning `null`
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function(String firstName, String lastName, int age, String email,
+            String phone, String address, List<String> photos, String resume)?
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Basic() when $default != null:
+        return $default(_that.firstName, _that.lastName, _that.age, _that.email,
+            _that.phone, _that.address, _that.photos, _that.resume);
+      case _:
+        return null;
+    }
+  }
+}
+
 /// @nodoc
 @JsonSerializable()
-class _$BasicImpl implements _Basic {
-  const _$BasicImpl(
+class _Basic implements Basic {
+  const _Basic(
       {required this.firstName,
       required this.lastName,
       required this.age,
@@ -205,9 +314,7 @@ class _$BasicImpl implements _Basic {
       required final List<String> photos,
       required this.resume})
       : _photos = photos;
-
-  factory _$BasicImpl.fromJson(Map<String, dynamic> json) =>
-      _$$BasicImplFromJson(json);
+  factory _Basic.fromJson(Map<String, dynamic> json) => _$BasicFromJson(json);
 
   @override
   final String firstName;
@@ -232,16 +339,26 @@ class _$BasicImpl implements _Basic {
   @override
   final String resume;
 
+  /// Create a copy of Basic
+  /// with the given fields replaced by the non-null parameter values.
   @override
-  String toString() {
-    return 'Basic(firstName: $firstName, lastName: $lastName, age: $age, email: $email, phone: $phone, address: $address, photos: $photos, resume: $resume)';
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @pragma('vm:prefer-inline')
+  _$BasicCopyWith<_Basic> get copyWith =>
+      __$BasicCopyWithImpl<_Basic>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$BasicToJson(
+      this,
+    );
   }
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$BasicImpl &&
+            other is _Basic &&
             (identical(other.firstName, firstName) ||
                 other.firstName == firstName) &&
             (identical(other.lastName, lastName) ||
@@ -259,56 +376,85 @@ class _$BasicImpl implements _Basic {
   int get hashCode => Object.hash(runtimeType, firstName, lastName, age, email,
       phone, address, const DeepCollectionEquality().hash(_photos), resume);
 
-  /// Create a copy of Basic
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
   @override
-  @pragma('vm:prefer-inline')
-  _$$BasicImplCopyWith<_$BasicImpl> get copyWith =>
-      __$$BasicImplCopyWithImpl<_$BasicImpl>(this, _$identity);
-
-  @override
-  Map<String, dynamic> toJson() {
-    return _$$BasicImplToJson(
-      this,
-    );
+  String toString() {
+    return 'Basic(firstName: $firstName, lastName: $lastName, age: $age, email: $email, phone: $phone, address: $address, photos: $photos, resume: $resume)';
   }
 }
 
-abstract class _Basic implements Basic {
-  const factory _Basic(
-      {required final String firstName,
-      required final String lastName,
-      required final int age,
-      required final String email,
-      required final String phone,
-      required final String address,
-      required final List<String> photos,
-      required final String resume}) = _$BasicImpl;
+/// @nodoc
+abstract mixin class _$BasicCopyWith<$Res> implements $BasicCopyWith<$Res> {
+  factory _$BasicCopyWith(_Basic value, $Res Function(_Basic) _then) =
+      __$BasicCopyWithImpl;
+  @override
+  @useResult
+  $Res call(
+      {String firstName,
+      String lastName,
+      int age,
+      String email,
+      String phone,
+      String address,
+      List<String> photos,
+      String resume});
+}
 
-  factory _Basic.fromJson(Map<String, dynamic> json) = _$BasicImpl.fromJson;
+/// @nodoc
+class __$BasicCopyWithImpl<$Res> implements _$BasicCopyWith<$Res> {
+  __$BasicCopyWithImpl(this._self, this._then);
 
-  @override
-  String get firstName;
-  @override
-  String get lastName;
-  @override
-  int get age;
-  @override
-  String get email;
-  @override
-  String get phone;
-  @override
-  String get address;
-  @override
-  List<String> get photos;
-  @override
-  String get resume;
+  final _Basic _self;
+  final $Res Function(_Basic) _then;
 
   /// Create a copy of Basic
   /// with the given fields replaced by the non-null parameter values.
   @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$BasicImplCopyWith<_$BasicImpl> get copyWith =>
-      throw _privateConstructorUsedError;
+  @pragma('vm:prefer-inline')
+  $Res call({
+    Object? firstName = null,
+    Object? lastName = null,
+    Object? age = null,
+    Object? email = null,
+    Object? phone = null,
+    Object? address = null,
+    Object? photos = null,
+    Object? resume = null,
+  }) {
+    return _then(_Basic(
+      firstName: null == firstName
+          ? _self.firstName
+          : firstName // ignore: cast_nullable_to_non_nullable
+              as String,
+      lastName: null == lastName
+          ? _self.lastName
+          : lastName // ignore: cast_nullable_to_non_nullable
+              as String,
+      age: null == age
+          ? _self.age
+          : age // ignore: cast_nullable_to_non_nullable
+              as int,
+      email: null == email
+          ? _self.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      phone: null == phone
+          ? _self.phone
+          : phone // ignore: cast_nullable_to_non_nullable
+              as String,
+      address: null == address
+          ? _self.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      photos: null == photos
+          ? _self._photos
+          : photos // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      resume: null == resume
+          ? _self.resume
+          : resume // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
 }
+
+// dart format on
