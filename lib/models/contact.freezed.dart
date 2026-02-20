4f7a1c548ed194f5b993b54b6883b4bc0a6faa05
samitad2024 <samigo2024@gmail.com>
2026-02-20 23:58:35 +0300

    chore: update lib/models/contact.freezed.dart

--- lib/models/contact.freezed.dart
+++ lib/models/contact.freezed.dart
@@ -1,5 +1,5 @@
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -9,160 +9,31 @@ part of 'contact.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
-
-Contact _$ContactFromJson(Map<String, dynamic> json) {
-  return _Contact.fromJson(json);
-}
 
 /// @nodoc
 mixin _$Contact {
-  String get icon => throw _privateConstructorUsedError;
-  String get title => throw _privateConstructorUsedError;
-  String get description => throw _privateConstructorUsedError;
-  String get action => throw _privateConstructorUsedError;
-
-  /// Serializes this Contact to a JSON map.
-  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  String get icon;
+  String get title;
+  String get description;
+  String get action;
 
   /// Create a copy of Contact
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $ContactCopyWith<Contact> get copyWith => throw _privateConstructorUsedError;
-}
-
-/// @nodoc
-abstract class $ContactCopyWith<$Res> {
-  factory $ContactCopyWith(Contact value, $Res Function(Contact) then) =
-      _$ContactCopyWithImpl<$Res, Contact>;
-  @useResult
-  $Res call({String icon, String title, String description, String action});
-}
-
-/// @nodoc
-class _$ContactCopyWithImpl<$Res, $Val extends Contact>
-    implements $ContactCopyWith<$Res> {
-  _$ContactCopyWithImpl(this._value, this._then);
-
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
-
-  /// Create a copy of Contact
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? icon = null,
-    Object? title = null,
-    Object? description = null,
-    Object? action = null,
-  }) {
-    return _then(_value.copyWith(
-      icon: null == icon
-          ? _value.icon
-          : icon // ignore: cast_nullable_to_non_nullable
-              as String,
-      title: null == title
-          ? _value.title
-          : title // ignore: cast_nullable_to_non_nullable
-              as String,
-      description: null == description
-          ? _value.description
-          : description // ignore: cast_nullable_to_non_nullable
-              as String,
-      action: null == action
-          ? _value.action
-          : action // ignore: cast_nullable_to_non_nullable
-              as String,
-    ) as $Val);
-  }
-}
+  $ContactCopyWith<Contact> get copyWith =>
+      _$ContactCopyWithImpl<Contact>(this as Contact, _$identity);
 
-/// @nodoc
-abstract class _$$ContactImplCopyWith<$Res> implements $ContactCopyWith<$Res> {
-  factory _$$ContactImplCopyWith(
-          _$ContactImpl value, $Res Function(_$ContactImpl) then) =
-      __$$ContactImplCopyWithImpl<$Res>;
-  @override
-  @useResult
-  $Res call({String icon, String title, String description, String action});
-}
-
-/// @nodoc
-class __$$ContactImplCopyWithImpl<$Res>
-    extends _$ContactCopyWithImpl<$Res, _$ContactImpl>
-    implements _$$ContactImplCopyWith<$Res> {
-  __$$ContactImplCopyWithImpl(
-      _$ContactImpl _value, $Res Function(_$ContactImpl) _then)
-      : super(_value, _then);
-
-  /// Create a copy of Contact
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? icon = null,
-    Object? title = null,
-    Object? description = null,
-    Object? action = null,
-  }) {
-    return _then(_$ContactImpl(
-      icon: null == icon
-          ? _value.icon
-          : icon // ignore: cast_nullable_to_non_nullable
-              as String,
-      title: null == title
-          ? _value.title
-          : title // ignore: cast_nullable_to_non_nullable
-              as String,
-      description: null == description
-          ? _value.description
-          : description // ignore: cast_nullable_to_non_nullable
-              as String,
-      action: null == action
-          ? _value.action
-          : action // ignore: cast_nullable_to_non_nullable
-              as String,
-    ));
-  }
-}
-
-/// @nodoc
-@JsonSerializable()
-class _$ContactImpl implements _Contact {
-  const _$ContactImpl(
-      {required this.icon,
-      required this.title,
-      required this.description,
-      required this.action});
-
-  factory _$ContactImpl.fromJson(Map<String, dynamic> json) =>
-      _$$ContactImplFromJson(json);
-
-  @override
-  final String icon;
-  @override
-  final String title;
-  @override
-  final String description;
-  @override
-  final String action;
-
-  @override
-  String toString() {
-    return 'Contact(icon: $icon, title: $title, description: $description, action: $action)';
-  }
+  /// Serializes this Contact to a JSON map.
+  Map<String, dynamic> toJson();
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$ContactImpl &&
+            other is Contact &&
             (identical(other.icon, icon) || other.icon == icon) &&
             (identical(other.title, title) || other.title == title) &&
             (identical(other.description, description) ||
@@ -175,44 +46,327 @@ class _$ContactImpl implements _Contact {
   int get hashCode =>
       Object.hash(runtimeType, icon, title, description, action);
 
-  /// Create a copy of Contact
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
   @override
-  @pragma('vm:prefer-inline')
-  _$$ContactImplCopyWith<_$ContactImpl> get copyWith =>
-      __$$ContactImplCopyWithImpl<_$ContactImpl>(this, _$identity);
-
-  @override
-  Map<String, dynamic> toJson() {
-    return _$$ContactImplToJson(
-      this,
-    );
+  String toString() {
+    return 'Contact(icon: $icon, title: $title, description: $description, action: $action)';
   }
 }
 
-abstract class _Contact implements Contact {
-  const factory _Contact(
-      {required final String icon,
-      required final String title,
-      required final String description,
-      required final String action}) = _$ContactImpl;
+/// @nodoc
+abstract mixin class $ContactCopyWith<$Res> {
+  factory $ContactCopyWith(Contact value, $Res Function(Contact) _then) =
+      _$ContactCopyWithImpl;
+  @useResult
+  $Res call({String icon, String title, String description, String action});
+}
 
-  factory _Contact.fromJson(Map<String, dynamic> json) = _$ContactImpl.fromJson;
+/// @nodoc
+class _$ContactCopyWithImpl<$Res> implements $ContactCopyWith<$Res> {
+  _$ContactCopyWithImpl(this._self, this._then);
+
+  final Contact _self;
+  final $Res Function(Contact) _then;
+
+  /// Create a copy of Contact
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? icon = null,
+    Object? title = null,
+    Object? description = null,
+    Object? action = null,
+  }) {
+    return _then(_self.copyWith(
+      icon: null == icon
+          ? _self.icon
+          : icon // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _self.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _self.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      action: null == action
+          ? _self.action
+          : action // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// Adds pattern-matching-related methods to [Contact].
+extension ContactPatterns on Contact {
+  /// A variant of `map` that fallback to returning `orElse`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_Contact value)? $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _Contact() when $default != null:
+        return $default(_that);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// Callbacks receives the raw object, upcasted.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case final Subclass2 value:
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_Contact value) $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Contact():
+        return $default(_that);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `map` that fallback to returning `null`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_Contact value)? $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Contact() when $default != null:
+        return $default(_that);
+      case _:
+        return null;
+    }
+  }
+
+  /// A variant of `when` that fallback to an `orElse` callback.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function(
+            String icon, String title, String description, String action)?
+        $default, {
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case _Contact() when $default != null:
+        return $default(
+            _that.icon, _that.title, _that.description, _that.action);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// As opposed to `map`, this offers destructuring.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case Subclass2(:final field2):
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function(
+            String icon, String title, String description, String action)
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Contact():
+        return $default(
+            _that.icon, _that.title, _that.description, _that.action);
+      case _:
+        throw StateError('Unexpected subclass');
+    }
+  }
+
+  /// A variant of `when` that fallback to returning `null`
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function(
+            String icon, String title, String description, String action)?
+        $default,
+  ) {
+    final _that = this;
+    switch (_that) {
+      case _Contact() when $default != null:
+        return $default(
+            _that.icon, _that.title, _that.description, _that.action);
+      case _:
+        return null;
+    }
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _Contact implements Contact {
+  const _Contact(
+      {required this.icon,
+      required this.title,
+      required this.description,
+      required this.action});
+  factory _Contact.fromJson(Map<String, dynamic> json) =>
+      _$ContactFromJson(json);
 
   @override
-  String get icon;
+  final String icon;
   @override
-  String get title;
+  final String title;
   @override
-  String get description;
+  final String description;
   @override
-  String get action;
+  final String action;
 
   /// Create a copy of Contact
   /// with the given fields replaced by the non-null parameter values.
   @override
   @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$ContactImplCopyWith<_$ContactImpl> get copyWith =>
-      throw _privateConstructorUsedError;
+  @pragma('vm:prefer-inline')
+  _$ContactCopyWith<_Contact> get copyWith =>
+      __$ContactCopyWithImpl<_Contact>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$ContactToJson(
+      this,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _Contact &&
+            (identical(other.icon, icon) || other.icon == icon) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.action, action) || other.action == action));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, icon, title, description, action);
+
+  @override
+  String toString() {
+    return 'Contact(icon: $icon, title: $title, description: $description, action: $action)';
+  }
 }
+
+/// @nodoc
+abstract mixin class _$ContactCopyWith<$Res> implements $ContactCopyWith<$Res> {
+  factory _$ContactCopyWith(_Contact value, $Res Function(_Contact) _then) =
+      __$ContactCopyWithImpl;
+  @override
+  @useResult
+  $Res call({String icon, String title, String description, String action});
+}
+
+/// @nodoc
+class __$ContactCopyWithImpl<$Res> implements _$ContactCopyWith<$Res> {
+  __$ContactCopyWithImpl(this._self, this._then);
+
+  final _Contact _self;
+  final $Res Function(_Contact) _then;
+
+  /// Create a copy of Contact
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @pragma('vm:prefer-inline')
+  $Res call({
+    Object? icon = null,
+    Object? title = null,
+    Object? description = null,
+    Object? action = null,
+  }) {
+    return _then(_Contact(
+      icon: null == icon
+          ? _self.icon
+          : icon // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _self.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _self.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      action: null == action
+          ? _self.action
+          : action // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+// dart format on
