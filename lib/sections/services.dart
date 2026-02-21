9b7c79773ee5b112824b2301247765edc335bdea
samitad2024 <samigo2024@gmail.com>
2026-02-21 08:00:47 +0300

    chore: update lib/sections/services.dart

--- lib/sections/services.dart
+++ lib/sections/services.dart
@@ -1,34 +1,26 @@
 import 'package:devfolio/components/service_card.dart';
 import 'package:devfolio/models/service_data.dart';
 import 'package:jaspr/jaspr.dart';
+import 'package:jaspr/dom.dart';
 
 class ServicesSection extends StatelessComponent {
   final List<ServiceData> services;
-  const ServicesSection({
-    super.key,
-    required this.services,
-  });
+  const ServicesSection({super.key, required this.services});
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
     yield section(classes: 'services-section', [
-      span(classes: 'title', [
-        text('What I can do?'),
-      ]),
+      span(classes: 'title', [text('What I can do?')]),
       span(classes: 'subtitle', [
         text("I may not be perfect but surely I'm of some use :)"),
       ]),
       div(classes: 'section-body', id: 'services', [
         for (final service in services)
-          ServiceCard(
-            icon: service.icon,
-            label: service.title,
-          ),
-      ])
+          ServiceCard(icon: service.icon, label: service.title),
+      ]),
     ]);
   }
 
-  @css
   static final List<StyleRule> styles = [
     css('.services-section')
         .flexbox(
@@ -36,9 +28,7 @@ class ServicesSection extends StatelessComponent {
           alignItems: AlignItems.center,
           justifyContent: JustifyContent.start,
         )
-        .box(
-          padding: EdgeInsets.symmetric(vertical: 5.vh),
-        ),
+        .box(padding: EdgeInsets.symmetric(vertical: 5.vh)),
     css('.section-body')
         .flexbox(
           direction: FlexDirection.row,
