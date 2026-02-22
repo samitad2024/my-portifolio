0882f66e918e83c3e57f75ed652897f30f112022
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:52:04 +0300

    Update lib/sections/basic_info.dart

--- lib/sections/basic_info.dart
+++ lib/sections/basic_info.dart
@@ -15,8 +15,8 @@ class BasicInfoSection extends StatelessComponent {
   });
 
   @override
-  Iterable<Component> build(BuildContext context) sync* {
-    yield section(classes: 'info-section', [
+  Component build(BuildContext context) {
+    return section(classes: 'info-section', [
       div(classes: 'info', [
         div(classes: 'welcome', [
           span(classes: 'welcome-text', [text('WELCOME TO MY PORTFOLIO')]),
@@ -40,76 +40,78 @@ class BasicInfoSection extends StatelessComponent {
   }
 
   static final List<StyleRule> styles = [
-    css('.info-section')
-        .flexbox(
-          direction: FlexDirection.row,
-          alignItems: AlignItems.center,
-          justifyContent: JustifyContent.spaceBetween,
-        )
-        .box(padding: EdgeInsets.only(left: 10.vw)),
-    css('.welcome')
-        .box(margin: EdgeInsets.only(bottom: 2.5.vh))
-        .flexbox(alignItems: AlignItems.end),
-    css('.welcome-text').box(
-      padding: EdgeInsets.only(right: 12.px, bottom: 2.px),
+    css('.info-section').styles(
+      flexDirection: FlexDirection.row,
+      alignItems: AlignItems.center,
+      justifyContent: JustifyContent.spaceBetween,
+      padding: Padding.only(left: 10.vw),
     ),
-    css('.welcome-text', [
-      css.text(fontFamily: FontFamily('Montserrat'), fontSize: 20.px),
-    ]),
-    css('.first-name').box(padding: EdgeInsets.only(top: 3.vh)),
-    css('.first-name', [
-      css.text(
-        fontFamily: FontFamily('Montserrat'),
-        fontWeight: FontWeight.w100,
-        lineHeight: 3.vh,
-      ),
-    ]),
+    css('.welcome').styles(
+      margin: Margin.only(bottom: 2.5.vh),
+      alignItems: AlignItems.end,
+    ),
+    css('.welcome-text').styles(
+      padding: Padding.only(right: 12.px, bottom: 2.px),
+      fontFamily: FontFamily('Montserrat'),
+      fontSize: 20.px,
+    ),
+    css('.first-name').styles(
+      padding: Padding.only(top: 3.vh),
+      fontFamily: FontFamily('Montserrat'),
+      fontWeight: FontWeight.w100,
+      lineHeight: 3.vh,
+    ),
     css.fontFace(family: 'PoppinsBold', url: '/fonts/poppins/Poppins-Bold.ttf'),
-    css('.last-name', [
-      css.text(
-        fontFamily: FontFamily('PoppinsBold'),
-        fontWeight: FontWeight.bolder,
-      ),
-    ]),
-    css('.main-image')
-        .box(height: 85.vh, width: 75.vh)
-        .background(
-          image: ImageStyle.url(StaticAssets.bwImage),
-          size: BackgroundSize.cover,
-        ),
-    css('.play-icon', [css.text(color: themePrimaryColor)]),
+    css('.last-name').styles(
+      fontFamily: FontFamily('PoppinsBold'),
+      fontWeight: FontWeight.bolder,
+    ),
+    css('.main-image').styles(
+      height: 85.vh,
+      width: 75.vh,
+      backgroundImage: ImageStyle.url(StaticAssets.bwImage),
+      backgroundSize: BackgroundSize.cover,
+    ),
+    css('.play-icon').styles(color: themePrimaryColor),
     css(
       '.subtitle',
-    ).flexbox(direction: FlexDirection.row, alignItems: AlignItems.center),
-    css('.dynamic-subtitles').box(margin: EdgeInsets.only(left: 15.px)),
-    css('.dynamic-subtitles', [css.text(fontSize: 18.px)]),
-    css('.socials')
-        .box(padding: EdgeInsets.only(top: 2.vh))
-        .flexbox(direction: FlexDirection.row, alignItems: AlignItems.center),
-    css('.social-icon').box(padding: EdgeInsets.only(right: 25.px)),
-    css('.social-icon', [css.text(fontSize: 25.px, color: Colors.white)]),
-    css(
-      '.social-icon:hover',
-    ).box(transition: Transition('color', duration: 300)),
-    css('.social-icon:hover', [css.text(color: themePrimaryColor)]),
+    ).styles(flexDirection: FlexDirection.row, alignItems: AlignItems.center),
+    css('.dynamic-subtitles').styles(
+      margin: Margin.only(left: 15.px),
+      fontSize: 18.px,
+    ),
+    css('.socials').styles(
+      padding: Padding.only(top: 2.vh),
+      flexDirection: FlexDirection.row,
+      alignItems: AlignItems.center,
+    ),
+    css('.social-icon').styles(
+      padding: Padding.only(right: 25.px),
+      fontSize: 25.px,
+      color: Colors.white,
+    ),
+    css('.social-icon:hover').styles(
+      transition: Transition('color', duration: Duration(milliseconds: 300)),
+      color: themePrimaryColor,
+    ),
 
     /// MEDIA QUERY 1100px
     css.media(MediaQuery.screen(maxWidth: 1100.px), [
-      css('.welcome-text', [css.text(fontSize: 15.px)]),
-      css('.first-name', [css.text(fontSize: 50.px)]),
-      css('.last-name', [css.text(fontSize: 50.px)]),
-      css('.dynamic-subtitles', [css.text(fontSize: 14.px)]),
-      css('.main-image').box(height: 65.vh, width: 55.vh),
-      css('.play-icon').box(height: 15.px),
+      css('.welcome-text').styles(fontSize: 15.px),
+      css('.first-name').styles(fontSize: 50.px),
+      css('.last-name').styles(fontSize: 50.px),
+      css('.dynamic-subtitles').styles(fontSize: 14.px),
+      css('.main-image').styles(height: 65.vh, width: 55.vh),
+      css('.play-icon').styles(height: 15.px),
     ]),
 
     /// MEDIA QUERY 786px
     css.media(MediaQuery.screen(maxWidth: 786.px), [
-      css('.info-section').flexbox(direction: FlexDirection.column),
+      css('.info-section').styles(flexDirection: FlexDirection.column),
       css(
         '.main-image',
-      ).box(height: 55.vh, width: 45.vh).flexItem(alignSelf: AlignSelf.end),
-      css('.info').flexItem(alignSelf: AlignSelf.start),
+      ).styles(height: 55.vh, width: 45.vh, alignSelf: AlignSelf.end),
+      css('.info').styles(alignSelf: AlignSelf.start),
     ]),
   ];
 }
