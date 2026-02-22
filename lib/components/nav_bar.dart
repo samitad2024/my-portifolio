3e92eace106a294b0393ad2364ba904e8d8c8d0b
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:50:06 +0300

    Update lib/components/nav_bar.dart

--- lib/components/nav_bar.dart
+++ lib/components/nav_bar.dart
@@ -1,15 +1,16 @@
 import 'package:devfolio/components/app_button.dart';
 import 'package:devfolio/constants/theme.dart';
-import 'package:jaspr/jaspr.dart';
 import 'package:jaspr/dom.dart';
+import 'package:jaspr/jaspr.dart';
+import 'package:jaspr/src/dom/styles/css.dart';
 
 @client
 class NavBar extends StatelessComponent {
   const NavBar({super.key});
 
   @override
-  Iterable<Component> build(BuildContext context) sync* {
-    yield section(classes: 'navbar', [
+  Component build(BuildContext context) {
+    return section(classes: 'navbar', [
       div([
         span([text('< ')]),
         span(classes: 'brand', [text('Hamza')]),
@@ -31,28 +32,29 @@ class NavBar extends StatelessComponent {
   }
 
   static final List<StyleRule> styles = [
-    css('.navbar')
-        .box(padding: EdgeInsets.all(25.px))
-        .flexbox(
-          direction: FlexDirection.row,
-          justifyContent: JustifyContent.spaceBetween,
-          alignItems: AlignItems.center,
-        )
-        .background(color: Colors.black),
-    css('.navbar-label')
-        .box(padding: EdgeInsets.only(right: 35.px))
-        .text(fontSize: 12.px, decoration: TextDecoration.none)
-        .flexbox(
-          direction: FlexDirection.row,
-          justifyContent: JustifyContent.spaceBetween,
-          alignItems: AlignItems.center,
-        ),
-    css('.navbar-label:hover')
-        .text(color: themePrimaryColor)
-        .box(transition: Transition('color', duration: 500)),
-    css(
-      '.labels',
-    ).flexbox(direction: FlexDirection.row, justifyContent: JustifyContent.end),
-    css('.brand').text(fontFamily: FontFamily('Agustina'), fontSize: 24.px),
+    css('.navbar').styles(
+      padding: Padding.all(25.px),
+      flexDirection: FlexDirection.row,
+      justifyContent: JustifyContent.spaceBetween,
+      alignItems: AlignItems.center,
+      backgroundColor: Colors.black,
+    ),
+    css('.navbar-label').styles(
+      padding: Padding.only(right: 35.px),
+      fontSize: 12.px,
+      textDecoration: TextDecoration.none,
+      flexDirection: FlexDirection.row,
+      justifyContent: JustifyContent.spaceBetween,
+      alignItems: AlignItems.center,
+    ),
+    css('.navbar-label:hover').styles(
+      color: themePrimaryColor,
+      transition: Transition('color', duration: Duration(milliseconds: 500)),
+    ),
+    css('.labels').styles(
+      flexDirection: FlexDirection.row,
+      justifyContent: JustifyContent.end,
+    ),
+    css('.brand').styles(fontFamily: FontFamily('Agustina'), fontSize: 24.px),
   ];
 }
