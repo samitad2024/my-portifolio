f0483bd63bf2a210d06b2c3183a92e718d9c0bc5
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:51:29 +0300

    Update lib/models/service_data.g.dart

--- lib/models/service_data.g.dart
+++ lib/models/service_data.g.dart
@@ -6,13 +6,8 @@ part of 'service_data.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_ServiceData _$ServiceDataFromJson(Map<String, dynamic> json) => _ServiceData(
-      icon: json['icon'] as String,
-      title: json['title'] as String,
-    );
+_ServiceData _$ServiceDataFromJson(Map<String, dynamic> json) =>
+    _ServiceData(icon: json['icon'] as String, title: json['title'] as String);
 
 Map<String, dynamic> _$ServiceDataToJson(_ServiceData instance) =>
-    <String, dynamic>{
-      'icon': instance.icon,
-      'title': instance.title,
-    };
+    <String, dynamic>{'icon': instance.icon, 'title': instance.title};
