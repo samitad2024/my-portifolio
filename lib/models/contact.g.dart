eab95d7817f988d9e2d555fb8d10fb42a5dc4eb8
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:51:01 +0300

    Update lib/models/contact.g.dart

--- lib/models/contact.g.dart
+++ lib/models/contact.g.dart
@@ -7,15 +7,15 @@ part of 'contact.dart';
 // **************************************************************************
 
 _Contact _$ContactFromJson(Map<String, dynamic> json) => _Contact(
-      icon: json['icon'] as String,
-      title: json['title'] as String,
-      description: json['description'] as String,
-      action: json['action'] as String,
-    );
+  icon: json['icon'] as String,
+  title: json['title'] as String,
+  description: json['description'] as String,
+  action: json['action'] as String,
+);
 
 Map<String, dynamic> _$ContactToJson(_Contact instance) => <String, dynamic>{
-      'icon': instance.icon,
-      'title': instance.title,
-      'description': instance.description,
-      'action': instance.action,
-    };
+  'icon': instance.icon,
+  'title': instance.title,
+  'description': instance.description,
+  'action': instance.action,
+};
