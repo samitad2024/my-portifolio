3546d5a78604c283bc63cc995eaf72f85910cc84
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:50:21 +0300

    Update lib/constants/theme.dart

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -1,7 +1,6 @@
-import 'package:jaspr/jaspr.dart';
 import 'package:jaspr/dom.dart';
 
 // As your css styles are defined using just Dart, you can simply
 // use global variables or methods for common things like colors.
-const themePrimaryColor = Color.hex('#C0392B');
-const themeDarkGreyColor = Color.hex('#212121');
+const themePrimaryColor = Color('#C0392B');
+const themeDarkGreyColor = Color('#212121');
