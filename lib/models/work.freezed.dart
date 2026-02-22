cca6942551bf89560454f63cf7a490d0090d9d9b
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:51:43 +0300

    Update lib/models/work.freezed.dart

--- lib/models/work.freezed.dart
+++ lib/models/work.freezed.dart
@@ -14,326 +14,270 @@ T _$identity<T>(T value) => value;
 
 /// @nodoc
 mixin _$Work {
-  String get image;
-  String get url;
-  int get height;
 
-  /// Create a copy of Work
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @pragma('vm:prefer-inline')
-  $WorkCopyWith<Work> get copyWith =>
-      _$WorkCopyWithImpl<Work>(this as Work, _$identity);
+ String get image; String get url; int get height;
+/// Create a copy of Work
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$WorkCopyWith<Work> get copyWith => _$WorkCopyWithImpl<Work>(this as Work, _$identity);
 
   /// Serializes this Work to a JSON map.
   Map<String, dynamic> toJson();
 
-  @override
-  bool operator ==(Object other) {
-    return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is Work &&
-            (identical(other.image, image) || other.image == image) &&
-            (identical(other.url, url) || other.url == url) &&
-            (identical(other.height, height) || other.height == height));
-  }
 
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @override
-  int get hashCode => Object.hash(runtimeType, image, url, height);
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Work&&(identical(other.image, image) || other.image == image)&&(identical(other.url, url) || other.url == url)&&(identical(other.height, height) || other.height == height));
+}
 
-  @override
-  String toString() {
-    return 'Work(image: $image, url: $url, height: $height)';
-  }
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,image,url,height);
+
+@override
+String toString() {
+  return 'Work(image: $image, url: $url, height: $height)';
+}
+
+
 }
 
 /// @nodoc
-abstract mixin class $WorkCopyWith<$Res> {
-  factory $WorkCopyWith(Work value, $Res Function(Work) _then) =
-      _$WorkCopyWithImpl;
-  @useResult
-  $Res call({String image, String url, int height});
-}
+abstract mixin class $WorkCopyWith<$Res>  {
+  factory $WorkCopyWith(Work value, $Res Function(Work) _then) = _$WorkCopyWithImpl;
+@useResult
+$Res call({
+ String image, String url, int height
+});
 
+
+
+
+}
 /// @nodoc
-class _$WorkCopyWithImpl<$Res> implements $WorkCopyWith<$Res> {
+class _$WorkCopyWithImpl<$Res>
+    implements $WorkCopyWith<$Res> {
   _$WorkCopyWithImpl(this._self, this._then);
 
   final Work _self;
   final $Res Function(Work) _then;
 
-  /// Create a copy of Work
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({
-    Object? image = null,
-    Object? url = null,
-    Object? height = null,
-  }) {
-    return _then(_self.copyWith(
-      image: null == image
-          ? _self.image
-          : image // ignore: cast_nullable_to_non_nullable
-              as String,
-      url: null == url
-          ? _self.url
-          : url // ignore: cast_nullable_to_non_nullable
-              as String,
-      height: null == height
-          ? _self.height
-          : height // ignore: cast_nullable_to_non_nullable
-              as int,
-    ));
-  }
+/// Create a copy of Work
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? image = null,Object? url = null,Object? height = null,}) {
+  return _then(_self.copyWith(
+image: null == image ? _self.image : image // ignore: cast_nullable_to_non_nullable
+as String,url: null == url ? _self.url : url // ignore: cast_nullable_to_non_nullable
+as String,height: null == height ? _self.height : height // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
 }
+
+}
+
 
 /// Adds pattern-matching-related methods to [Work].
 extension WorkPatterns on Work {
-  /// A variant of `map` that fallback to returning `orElse`.
-  ///
-  /// It is equivalent to doing:
-  /// ```dart
-  /// switch (sealedClass) {
-  ///   case final Subclass value:
-  ///     return ...;
-  ///   case _:
-  ///     return orElse();
-  /// }
-  /// ```
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
 
-  @optionalTypeArgs
-  TResult maybeMap<TResult extends Object?>(
-    TResult Function(_Work value)? $default, {
-    required TResult orElse(),
-  }) {
-    final _that = this;
-    switch (_that) {
-      case _Work() when $default != null:
-        return $default(_that);
-      case _:
-        return orElse();
-    }
-  }
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _Work value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _Work() when $default != null:
+return $default(_that);case _:
+  return orElse();
 
-  /// A `switch`-like method, using callbacks.
-  ///
-  /// Callbacks receives the raw object, upcasted.
-  /// It is equivalent to doing:
-  /// ```dart
-  /// switch (sealedClass) {
-  ///   case final Subclass value:
-  ///     return ...;
-  ///   case final Subclass2 value:
-  ///     return ...;
-  /// }
-  /// ```
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
 
-  @optionalTypeArgs
-  TResult map<TResult extends Object?>(
-    TResult Function(_Work value) $default,
-  ) {
-    final _that = this;
-    switch (_that) {
-      case _Work():
-        return $default(_that);
-      case _:
-        throw StateError('Unexpected subclass');
-    }
-  }
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _Work value)  $default,){
+final _that = this;
+switch (_that) {
+case _Work():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
 
-  /// A variant of `map` that fallback to returning `null`.
-  ///
-  /// It is equivalent to doing:
-  /// ```dart
-  /// switch (sealedClass) {
-  ///   case final Subclass value:
-  ///     return ...;
-  ///   case _:
-  ///     return null;
-  /// }
-  /// ```
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
 
-  @optionalTypeArgs
-  TResult? mapOrNull<TResult extends Object?>(
-    TResult? Function(_Work value)? $default,
-  ) {
-    final _that = this;
-    switch (_that) {
-      case _Work() when $default != null:
-        return $default(_that);
-      case _:
-        return null;
-    }
-  }
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _Work value)?  $default,){
+final _that = this;
+switch (_that) {
+case _Work() when $default != null:
+return $default(_that);case _:
+  return null;
 
-  /// A variant of `when` that fallback to an `orElse` callback.
-  ///
-  /// It is equivalent to doing:
-  /// ```dart
-  /// switch (sealedClass) {
-  ///   case Subclass(:final field):
-  ///     return ...;
-  ///   case _:
-  ///     return orElse();
-  /// }
-  /// ```
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
 
-  @optionalTypeArgs
-  TResult maybeWhen<TResult extends Object?>(
-    TResult Function(String image, String url, int height)? $default, {
-    required TResult orElse(),
-  }) {
-    final _that = this;
-    switch (_that) {
-      case _Work() when $default != null:
-        return $default(_that.image, _that.url, _that.height);
-      case _:
-        return orElse();
-    }
-  }
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( String image,  String url,  int height)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _Work() when $default != null:
+return $default(_that.image,_that.url,_that.height);case _:
+  return orElse();
 
-  /// A `switch`-like method, using callbacks.
-  ///
-  /// As opposed to `map`, this offers destructuring.
-  /// It is equivalent to doing:
-  /// ```dart
-  /// switch (sealedClass) {
-  ///   case Subclass(:final field):
-  ///     return ...;
-  ///   case Subclass2(:final field2):
-  ///     return ...;
-  /// }
-  /// ```
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
 
-  @optionalTypeArgs
-  TResult when<TResult extends Object?>(
-    TResult Function(String image, String url, int height) $default,
-  ) {
-    final _that = this;
-    switch (_that) {
-      case _Work():
-        return $default(_that.image, _that.url, _that.height);
-      case _:
-        throw StateError('Unexpected subclass');
-    }
-  }
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( String image,  String url,  int height)  $default,) {final _that = this;
+switch (_that) {
+case _Work():
+return $default(_that.image,_that.url,_that.height);case _:
+  throw StateError('Unexpected subclass');
 
-  /// A variant of `when` that fallback to returning `null`
-  ///
-  /// It is equivalent to doing:
-  /// ```dart
-  /// switch (sealedClass) {
-  ///   case Subclass(:final field):
-  ///     return ...;
-  ///   case _:
-  ///     return null;
-  /// }
-  /// ```
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
 
-  @optionalTypeArgs
-  TResult? whenOrNull<TResult extends Object?>(
-    TResult? Function(String image, String url, int height)? $default,
-  ) {
-    final _that = this;
-    switch (_that) {
-      case _Work() when $default != null:
-        return $default(_that.image, _that.url, _that.height);
-      case _:
-        return null;
-    }
-  }
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( String image,  String url,  int height)?  $default,) {final _that = this;
+switch (_that) {
+case _Work() when $default != null:
+return $default(_that.image,_that.url,_that.height);case _:
+  return null;
+
+}
+}
+
 }
 
 /// @nodoc
 @JsonSerializable()
+
 class _Work implements Work {
   const _Work({required this.image, required this.url, required this.height});
   factory _Work.fromJson(Map<String, dynamic> json) => _$WorkFromJson(json);
 
-  @override
-  final String image;
-  @override
-  final String url;
-  @override
-  final int height;
+@override final  String image;
+@override final  String url;
+@override final  int height;
 
-  /// Create a copy of Work
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @pragma('vm:prefer-inline')
-  _$WorkCopyWith<_Work> get copyWith =>
-      __$WorkCopyWithImpl<_Work>(this, _$identity);
+/// Create a copy of Work
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$WorkCopyWith<_Work> get copyWith => __$WorkCopyWithImpl<_Work>(this, _$identity);
 
-  @override
-  Map<String, dynamic> toJson() {
-    return _$WorkToJson(
-      this,
-    );
-  }
+@override
+Map<String, dynamic> toJson() {
+  return _$WorkToJson(this, );
+}
 
-  @override
-  bool operator ==(Object other) {
-    return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is _Work &&
-            (identical(other.image, image) || other.image == image) &&
-            (identical(other.url, url) || other.url == url) &&
-            (identical(other.height, height) || other.height == height));
-  }
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Work&&(identical(other.image, image) || other.image == image)&&(identical(other.url, url) || other.url == url)&&(identical(other.height, height) || other.height == height));
+}
 
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @override
-  int get hashCode => Object.hash(runtimeType, image, url, height);
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,image,url,height);
 
-  @override
-  String toString() {
-    return 'Work(image: $image, url: $url, height: $height)';
-  }
+@override
+String toString() {
+  return 'Work(image: $image, url: $url, height: $height)';
+}
+
+
 }
 
 /// @nodoc
 abstract mixin class _$WorkCopyWith<$Res> implements $WorkCopyWith<$Res> {
-  factory _$WorkCopyWith(_Work value, $Res Function(_Work) _then) =
-      __$WorkCopyWithImpl;
-  @override
-  @useResult
-  $Res call({String image, String url, int height});
-}
+  factory _$WorkCopyWith(_Work value, $Res Function(_Work) _then) = __$WorkCopyWithImpl;
+@override @useResult
+$Res call({
+ String image, String url, int height
+});
 
+
+
+
+}
 /// @nodoc
-class __$WorkCopyWithImpl<$Res> implements _$WorkCopyWith<$Res> {
+class __$WorkCopyWithImpl<$Res>
+    implements _$WorkCopyWith<$Res> {
   __$WorkCopyWithImpl(this._self, this._then);
 
   final _Work _self;
   final $Res Function(_Work) _then;
 
-  /// Create a copy of Work
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @pragma('vm:prefer-inline')
-  $Res call({
-    Object? image = null,
-    Object? url = null,
-    Object? height = null,
-  }) {
-    return _then(_Work(
-      image: null == image
-          ? _self.image
-          : image // ignore: cast_nullable_to_non_nullable
-              as String,
-      url: null == url
-          ? _self.url
-          : url // ignore: cast_nullable_to_non_nullable
-              as String,
-      height: null == height
-          ? _self.height
-          : height // ignore: cast_nullable_to_non_nullable
-              as int,
-    ));
-  }
+/// Create a copy of Work
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? image = null,Object? url = null,Object? height = null,}) {
+  return _then(_Work(
+image: null == image ? _self.image : image // ignore: cast_nullable_to_non_nullable
+as String,url: null == url ? _self.url : url // ignore: cast_nullable_to_non_nullable
+as String,height: null == height ? _self.height : height // ignore: cast_nullable_to_non_nullable
+as int,
+  ));
+}
+
+
 }
 
 // dart format on
