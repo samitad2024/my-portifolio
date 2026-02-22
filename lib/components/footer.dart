4fa73a311a41c102718391a6d0996b044c4fc397
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:50:01 +0300

    Update lib/components/footer.dart

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -6,30 +6,32 @@ class Footer extends StatelessComponent {
   const Footer({super.key});
 
   @override
-  Iterable<Component> build(BuildContext context) sync* {
-    yield footer(classes: 'foot', [
+  Component build(BuildContext context) {
+    return footer(classes: 'foot', [
       span([text('Developed in 💙 with')]),
       a(
         href: 'https://docs.page/schultek/jaspr',
         classes: 'tech-name',
         target: Target.blank,
-        styles: Styles.text(color: Color.hex('#1977d1')),
+        styles: Styles(color: Color('#1977d1')),
         [text('Jaspr')],
       ),
     ]);
   }
 
   static final List<StyleRule> styles = [
-    css('.foot')
-        .flexbox(
-          direction: FlexDirection.row,
-          alignItems: AlignItems.center,
-          justifyContent: JustifyContent.center,
-        )
-        .box(padding: EdgeInsets.symmetric(vertical: 2.vh))
-        .text(fontSize: 12.px),
-    css('.tech-name')
-        .text(fontSize: 12.px, decoration: TextDecoration.none)
-        .box(margin: EdgeInsets.only(left: 5.px)),
+    css('.foot').styles(
+      display: Display.flex,
+      padding: Padding.symmetric(vertical: 2.vh),
+      flexDirection: FlexDirection.row,
+      alignItems: AlignItems.center,
+      justifyContent: JustifyContent.center,
+      fontSize: 12.px,
+    ),
+    css('.tech-name').styles(
+      fontSize: 12.px,
+      textDecoration: TextDecoration.none,
+      margin: Margin.only(left: 5.px),
+    ),
   ];
 }
