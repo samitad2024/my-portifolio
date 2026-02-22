fa71a4b2586f7a0f6b5ab0302ac8b54c5d6054e7
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:50:40 +0300

    Update lib/models/about.g.dart

--- lib/models/about.g.dart
+++ lib/models/about.g.dart
@@ -7,17 +7,17 @@ part of 'about.dart';
 // **************************************************************************
 
 _About _$AboutFromJson(Map<String, dynamic> json) => _About(
-      description: json['description'] as String,
-      tech: (json['tech'] as List<dynamic>).map((e) => e as String).toList(),
-      works: (json['works'] as List<dynamic>)
-          .map((e) => Work.fromJson(e as Map<String, dynamic>))
-          .toList(),
-      heading: json['heading'] as String,
-    );
+  description: json['description'] as String,
+  tech: (json['tech'] as List<dynamic>).map((e) => e as String).toList(),
+  works: (json['works'] as List<dynamic>)
+      .map((e) => Work.fromJson(e as Map<String, dynamic>))
+      .toList(),
+  heading: json['heading'] as String,
+);
 
 Map<String, dynamic> _$AboutToJson(_About instance) => <String, dynamic>{
-      'description': instance.description,
-      'tech': instance.tech,
-      'works': instance.works.map((e) => e.toJson()).toList(),
-      'heading': instance.heading,
-    };
+  'description': instance.description,
+  'tech': instance.tech,
+  'works': instance.works.map((e) => e.toJson()).toList(),
+  'heading': instance.heading,
+};
