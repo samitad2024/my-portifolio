517498656b4d9e5754f5591447a0adac97742671
samitad2024 <samigo2024@gmail.com>
2026-02-22 23:50:52 +0300

    Update lib/models/basic.g.dart

--- lib/models/basic.g.dart
+++ lib/models/basic.g.dart
@@ -7,24 +7,23 @@ part of 'basic.dart';
 // **************************************************************************
 
 _Basic _$BasicFromJson(Map<String, dynamic> json) => _Basic(
-      firstName: json['firstName'] as String,
-      lastName: json['lastName'] as String,
-      age: (json['age'] as num).toInt(),
-      email: json['email'] as String,
-      phone: json['phone'] as String,
-      address: json['address'] as String,
-      photos:
-          (json['photos'] as List<dynamic>).map((e) => e as String).toList(),
-      resume: json['resume'] as String,
-    );
+  firstName: json['firstName'] as String,
+  lastName: json['lastName'] as String,
+  age: (json['age'] as num).toInt(),
+  email: json['email'] as String,
+  phone: json['phone'] as String,
+  address: json['address'] as String,
+  photos: (json['photos'] as List<dynamic>).map((e) => e as String).toList(),
+  resume: json['resume'] as String,
+);
 
 Map<String, dynamic> _$BasicToJson(_Basic instance) => <String, dynamic>{
-      'firstName': instance.firstName,
-      'lastName': instance.lastName,
-      'age': instance.age,
-      'email': instance.email,
-      'phone': instance.phone,
-      'address': instance.address,
-      'photos': instance.photos,
-      'resume': instance.resume,
-    };
+  'firstName': instance.firstName,
+  'lastName': instance.lastName,
+  'age': instance.age,
+  'email': instance.email,
+  'phone': instance.phone,
+  'address': instance.address,
+  'photos': instance.photos,
+  'resume': instance.resume,
+};
